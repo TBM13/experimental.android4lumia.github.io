e2f8c670043d898f39d42e6dcfad57dfe16f427a
TBM <mateo.trueba@gmail.com>
2020-01-10 15:36:36 -0300

    Create home.js

---
+++ assets/js/home.js
@@ -0,0 +1,23 @@
+(function() {
+    'use strict';
+    //Burger menu start
+    var bStatus = false;
+
+    function bMenuToggle() {
+      var bMenu = document.getElementById("bPanel");
+      var content = document.getElementById("content");
+      var pageheader = document.getElementById("pageheader");
+      document.getElementById("bBtn").classList.toggle("change");
+      if (!bStatus) {
+        bMenu.className += " opened";
+        content.setAttribute("onclick", "bMenuToggle()");
+        pageheader.setAttribute("onclick", "bMenuToggle()");
+      } else {
+        bMenu.className = "panel-wrap";
+        content.removeAttribute("onclick");
+        pageheader.removeAttribute("onclick");
+      }
+        bStatus = !bStatus;
+    }
+    //Burger menu end
+})();
